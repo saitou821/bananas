5aef0e52238739c4a7f804f8e6415d1d902a61ca
rika <krkr00821@icloud.com>
2023-11-01 21:26:38 +0900

    変更後のメッセージ

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\TestController;
  Route::get('/', function () {
      return view('welcome');
 });
- Route::get(‘/‘, [TestController::class, 'index']);
+ Route::get('/', [TestController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +19,7 @@ use App\Http\Controllers\TestController;
 |
 */
 
- Route::get(‘/‘,  function () {
+ Route::get('/',  function () {
     return view('welcome');
 });
 
